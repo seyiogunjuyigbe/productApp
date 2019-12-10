9b6ac8c63cd0236f69e642a244db318f7718b098
unknown <seyiogunjuyigbe@gmail.com>
2019-12-10 13:19:58 +0100

    int

--- src/app.js
+++ src/app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const faker = require('faker');
 const mongoose = require('mongoose');
 const app = express();
+const path = require('path')
 // Displayed because this is a test app
-const DB_URI = "mongodb://localhost:27017/product"
+// const config = require('./config');
+const DB_URI = "mongodb+srv://user:user@cluster0-snlrs.mongodb.net/test?retryWrites=true&w=majority"
+const URL = 'http://localhost:3000';
+
 mongoose.Promise = global.Promise;
 mongoose.connect(DB_URI, {
     useNewUrlParser:true,
@@ -14,25 +18,35 @@ mongoose.connect(DB_URI, {
 );
 
 app.set('view engine', 'ejs');
-
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, '../public')));
 const Product = require('./model');
 
-Product.create({
-    name: 'Product',
-    price: 200
-}, (err,product)=>{
-    if(!err){
-        console.log(product)
-    } else{
-        console.log(err)
-    }
-})
+// Product.create({
+//     name: faker.commerce.productName(),
+//     price: Number(faker.commerce.price())
+// }, (err,product)=>{
+//     if(!err){
+//         console.log(product)
+//     } else{
+//         console.log(err)
+//     }
+// })
 Product.find({}, (err,products)=>{
     if(!err){
         console.log(products)
     } else{
         console.log(err)
     }
+})
+app.get('/', (req,res)=>{
+    res.render('test', {url: URL})
+
+})
+
+app.get('/test', (req,res)=>{
+    res.json({status: res.statusCode})
+
 })
 const port = process.env.PORT || 3000;
 app.listen(port, ()=>console.log(`Connected to ${port}`))
---
+++ src/config.js
@@ -0,0 +1,3 @@
+const dotenv = require('dotenv');
+dotenv.config();
+exports.config = {URL,DB_URI}
