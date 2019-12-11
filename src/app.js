1d08fd28b7ab59cdd4e09f43c59f7952f3694c27
Oluwaseyi Ogunjuyigbe <seyiogunjuyigbe@gmail.com>
2019-12-11 08:46:15 +0100

    deploy error

--- src/app.js
+++ src/app.js
@@ -39,13 +39,14 @@ const seed = ()=>{
 // seed();
 
 app.get('/', (req,res)=>{
-    Product.find({}, (err,products)=>{
-        if(!err){
-           return res.status(200).render('index', {products: products, url:URL})
-        } else{
-            return res.status(404).status({err:err.message})
-        }
-    })
+    res.send("Hello world")
+    // Product.find({}, (err,products)=>{
+    //     if(!err){
+    //        return res.status(200).render('index', {products: products, url:URL})
+    //     } else{
+    //         return res.status(404).status({err:err.message})
+    //     }
+    // })
 })
 app.get('/:prod/price', (req,res)=>{
     Product.findById(req.params.prod, (err,product)=>{
